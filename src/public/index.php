d0ba3d3796332ac75fbd25dd5ac850ed6fc15a76
Axel Gromat <axel.gromat@gmail.com>
2021-11-29 16:15:30 +0100

    changed a index.php misplaced

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,28 @@
 <?php
 
+    require '../src/Router.php';
+    require '../src/PaymentGateway/Paypal/Transaction.php';
 
-declare(strict_types=1);
+use App\PaymentGateway\Paypal\Transaction;
+use App\Router;
 
+    declare(strict_types=1);
 
+// echo '<pre>';
+// print_r($_SERVER);
+// echo '</pre>';
 
-// phpinfo();
-echo '<pre>';
-var_dump($_SERVER);
-echo '</pre>';
+spl_autoload_register(function ($class) {
+    var_dump($class);
+});
+
+$router = new Router();
+$transaction = new Transaction(250, 'Transaction');
+
+$router->register('/', function () {
+    echo 'Homepage';
+});
+
+$router->register('/transactions', function () {
+    echo 'Transaction page';
+});
