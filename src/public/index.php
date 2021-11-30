1f536f4cc000560db7ae4d0d70c6f06a280478b9
Axel Gromat <axel.gromat@gmail.com>
2021-11-30 01:19:27 +0100

    Changed images for a smaller/lighter one

--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 require '../vendor/autoload.php';
 
+spl_autoload_register(function ($class) {
+    $class = '..'.substr(str_replace('\\', '/', $class), 3).'.php';
+
+    if (!file_exists($class)) {
+        require $class;
+    }
+});
     // require '../Router.php';
     // require '../PaymentGateway/Paypal/Transaction.php';
 
@@ -13,14 +20,6 @@ use App\Router;
 // echo '<pre>';
 // print_r($_SERVER);
 // echo '</pre>';
-
-spl_autoload_register(function ($class) {
-    $class = '..'.substr(str_replace('\\', '/', $class), 3).'.php';
-
-    if (!file_exists($class)) {
-        require $class;
-    }
-});
 
 $router = new Router();
 $transaction = new Transaction(250, 'Transaction');
