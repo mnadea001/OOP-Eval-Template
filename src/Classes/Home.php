49bb2f6a0322119027f40cabfd33d9e2a63861f5
Axel Gromat <axel.gromat@gmail.com>
2021-11-30 12:41:04 +0100

    finished routing

---
+++ src/Classes/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+    declare(strict_types=1);
+
+namespace App\Classes;
+
+class Home
+{
+    public static function index()
+    {
+        return 'This is the homepage !';
+    }
+
+    public function alt()
+    {
+        return strrev('This is an alternate universe');
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -10,9 +10,10 @@ require __DIR__.'/../../vendor/autoload.php';
 
 $router = new \App\Router();
 
-$router->register('/', function () { echo 'Homepage'; });
+$router->register('/', [App\Classes\Home::class, 'index']);
+$router->register('/alt', [App\Classes\Home::class, 'alt']);
 $router->register('/hw', function () { require 'hello.php'; });
 $router->register('/transactions', function () { echo 'Transactions'; });
 
-$router->resolve($_SERVER['REQUEST_URI']);
+echo $router->resolve($_SERVER['REQUEST_URI']);
 // var_dump($router);
