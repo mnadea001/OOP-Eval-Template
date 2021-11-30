777a9ded467fafc65ce6d186fa157c2585f3cd05
Axel Gromat <axel.gromat@gmail.com>
2021-11-30 10:48:49 +0100

    Added autoload and explications

--- src/Router.php
+++ src/Router.php
@@ -6,10 +6,4 @@ namespace App;
 
 class Router
 {
-    public function register(string $route, callable $action): self
-    {
-        $this->routes[$route] = $action;
-
-        return $this;
-    }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -2,32 +2,12 @@
 
 declare(strict_types=1);
 
-require '../vendor/autoload.php';
-
-spl_autoload_register(function ($class) {
-    $class = '..'.substr(str_replace('\\', '/', $class), 3).'.php';
-
-    if (!file_exists($class)) {
-        require $class;
-    }
-});
-    // require '../Router.php';
-    // require '../PaymentGateway/Paypal/Transaction.php';
-
-use App\PaymentGateway\Paypal\Transaction;
-use App\Router;
+require __DIR__.'/../../vendor/autoload.php';
 
 // echo '<pre>';
 // print_r($_SERVER);
 // echo '</pre>';
 
-$router = new Router();
-$transaction = new Transaction(250, 'Transaction');
+$router = new \App\Router();
 
-$router->register('/', function () {
-    echo 'Homepage';
-});
-
-$router->register('/transactions', function () {
-    echo 'Transaction page';
-});
+var_dump($router);
